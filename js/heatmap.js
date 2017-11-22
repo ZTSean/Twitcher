46ebccf926684bcc8c65f4396e8d9020c38e5e37
Zixiao Wang <wang5586@umn.edu>
2017-11-22 01:06:41 -0500

    js folder with heatmap.js

---
+++ js/heatmap.js
@@ -0,0 +1,381 @@
+//===================================================
+// Helper functions
+//
+// calculate mean and standard deviation of the array
+function meanAndStd(array) {
+    var num = 0;
+    var l = array.length;
+    for (var i = 0; i < l; i++) {
+        num += array[i];
+    }
+    var avg = num / l;
+
+    var StdSum = 0;
+    for (var i = 0; i < l; i++) {
+        StdSum += Math.pow((array[i] - avg), 2);
+    }
+    var std = Math.sqrt(StdSum / l);
+    return [avg, std]
+}
+
+function DataProcessing(error, gdata) {
+	// detect whether it reads csv file correctly
+	if (error) throw error;
+
+    //final variables 
+    document.body.style.backgroundColor = "white";
+    //start and end data of general dataset
+    var StartTime = new Date("2016/9/8");
+    var EndTime = new Date("2017/9/8");
+
+    var HmapLength = 3 * 366;
+
+    var games = ["Overwatch", "CSGO", "PUBG", "Destiny", "Destiny2"];
+    var gameColors = ["#5F8316", "#98CA32", "#B3BF0D", "#F7D302", "#DAC134"];
+
+    //can be changed based on user selection
+    //selected start time and selected end time
+    var SSTime = new Date("2016/9/8");
+    var SETime = new Date("2017/9/8");
+
+    var HmapAvg = 0;
+    var HmapStd = 0;
+
+    var HMfont = "Helvetica";
+
+    function prepareDataForHeatmap(gData, attribute) {
+    	// when the end time is earlier than start time: swap
+        if (SETime < SSTime) {
+            var temp = SSTime;
+            SSTime = SETime;
+            SETime = temp;
+        }
+
+        // deal with corner case:
+        // 1. selected end time is later than overall data end time
+        // 2. selected start time is earlier than the overall data star time
+        if (SETime > EndTime) { SETime = EndTime; }
+        if (SSTime < StartTime) { SSTime = StartTime }
+
+
+        var startPt = parseInt((SSTime - StartTime) / 86400000);
+        var endPt = parseInt((SETime - StartTime) / 86400000);
+        var effN = 0;
+        var sum = 0;
+        var prdData = [];
+        var numArray = [];
+        for (j = 0; j < 5; j++) {
+            var line = [];
+            //console.log(gData[j])
+            for (i = startPt; i <= endPt; i++) {
+                var temp = parseInt(gData[j][i][attribute]);
+                if (temp != -1) { numArray.push(temp); }
+                line.push({ 
+                	value: temp, 
+                	row: i, col: j, 
+                	x: startPt + i, 
+                	date: gData[j][i]["Date"] });
+            }
+            //console.log(line)
+            prdData.push(line);
+        }
+
+        var temp2 = meanAndStd(numArray);
+        HmapAvg = temp2[0];
+        HmapStd = temp2[1];
+
+        //console.log(prdData)
+        return prdData
+    }
+
+    function prepareDataForStarPlot(gData) {
+        var propertyAry = [];
+        var valueAry = [];
+        for (var pN in gData) {
+            if ((pN != "Game Rank (Total)") &&
+                (pN != "Game Rank (Esports)") &&
+                (pN != "Game Updates") &&
+                (pN != "Events") &&
+                (pN != "Competitive Seasons") &&
+                (pN != "Esports Schedule") &&
+                (pN != "Date")) {
+
+                if ((gData[pN] != -1) && (gData[pN] != "Unavailable")) {
+                    propertyAry.push(pN);
+                    valueAry.push(gData[pN]);
+                }
+            }
+        }
+        console.log([propertyAry, valueAry])
+        return [propertyAry, valueAry];
+    }
+
+    function updateInfoBoxHMap(infoData, settings) {
+        var info = gdata[infoData.col][infoData.x];
+        console.log(info);
+    }
+
+    function plotHeatmap(gameData) {
+        /*var data = [
+             { score: 0.5, row: 0, col: 0 },
+             { score: 0.7, row: 0, col: 1 },
+             { score: 0.2, row: 1, col: 0 },
+             { score: 0.4, row: 1, col: 1 }
+         ];*/
+
+        //var data = gameData[0];
+
+        var nBloc = gameData[0].length
+
+        //height of each row in the heatmap
+        //width of each column in the heatmap
+        var h = 50, //rect height
+            eLh = 50, //event line height
+            LH = h + eLh, //total height for each game
+            labelWidth = h, //width of the game label
+            w = parseInt(HmapLength / nBloc), //rect width
+            startH = 10, //first bar height
+            totalH = LH * 5 + startH, //total height of the map
+            dateH = 0; //start height for date text
+        TandGPadding = 70; //space between text and graph
+
+        var colorLow = 'green',
+            colorMed = 'yellow',
+            colorHigh = 'red',
+            colorLine = "#9966ff",
+            colorUnavailable = "#ebebe0";
+
+        var margin = { top: 50, right: 80, bottom: 30, left: 5 },
+            width = 1500 - margin.left - margin.right,
+            height = 1000 - margin.top - margin.bottom;
+
+        console.log(HmapAvg);
+        console.log(HmapStd);
+
+        var colorScale = d3.scale.linear()
+            .domain([HmapAvg - 1.5 * HmapStd, HmapAvg, HmapAvg + 1.5 * HmapStd])
+            .range([colorLow, colorMed, colorHigh]);
+
+        var svg = d3.select("#heatmap").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var clickHold = -1;
+        //var savedX1 = -1;
+
+        //Draw heatmap for data of each game
+        for (i = 0; i < 5; i++) {
+            var heatMap = svg.selectAll(".heatmap")
+                .data(gameData[i], function(d) { return d.col + ':' + d.row; })
+                .enter().append("svg:rect")
+                .attr("x", function(d) { return d.row * w + labelWidth + TandGPadding; })
+                .attr("y", function(d) { return startH + d.col * LH; })
+                .attr("width", function(d) { return w; })
+                .attr("height", function(d) { return h; })
+                .style("fill", function(d) {
+                    if (d.value == -1) {
+                        return colorUnavailable;
+                    }
+                    return colorScale(d.value);
+                })
+
+                .on("mouseover", function(d) {
+                    svg.append("line")
+                        .attr("class", "mol")
+                        .attr("x1", d.row * w + labelWidth + TandGPadding)
+                        .attr("y1", startH)
+                        .attr("x2", d.row * w + labelWidth + TandGPadding + w / 2)
+                        .attr("y2", totalH)
+                        .attr("stroke-width", 2)
+                        .attr("stroke", "#9966ff")
+                        .attr("stroke-opacity", 0.8);
+                    svg.append("text")
+                        .attr("y", dateH)
+                        .attr("x", d.row * w + labelWidth + TandGPadding + w / 2)
+                        .attr("class", "hover")
+                        .attr("text-anchor", "middle")
+                        .attr("font-family", "Helvetica")
+                        .attr("font-size", "15px")
+                        .style("fill", colorLine)
+                        .text(d.date);
+
+                    if (clickHold == -1) {
+                        updateInfoBoxHMap(d, 0);
+                    }
+                })
+
+                .on("mouseout", function(d) {
+                    svg.select("line.mol").remove();
+                    svg.select("text.hover").remove();
+                    if (clickHold == -1) {
+                        updateInfoBoxHMap(d, -1);
+                    }
+                })
+
+                .on("click", function(d) {
+                    if (clickHold == -1) {
+                        svg.append("line")
+                            .attr("class", "cl")
+                            .attr("x1", d.row * w + labelWidth + TandGPadding)
+                            .attr("y1", startH)
+                            .attr("x2", d.row * w + labelWidth + TandGPadding + w / 2)
+                            .attr("y2", totalH)
+                            .attr("stroke-width", 2)
+                            .attr("stroke", colorLine)
+                            .attr("stroke-opacity", 0.8);
+                        svg.append("text")
+                            .attr("y", dateH)
+                            .attr("x", d.row * w + labelWidth + TandGPadding + w / 2)
+                            .attr("class", "click")
+                            .attr("text-anchor", "middle")
+                            .attr("font-family", HMfont)
+                            .attr("font-size", "15px")
+                            .style("fill", colorLine)
+                            .text(d.date);
+                        //savedX1 = d.row * w + labelWidth + TandGPadding;
+                        clickHold = 0;
+                        updateInfoBoxHMap(d, 1);
+
+                    } else if (clickHold == 0) {
+                        svg.append("line")
+                            .attr("class", "cl")
+                            .attr("x1", d.row * w + labelWidth + TandGPadding)
+                            .attr("y1", startH)
+                            .attr("x2", d.row * w + labelWidth + TandGPadding + w / 2)
+                            .attr("y2", totalH)
+                            .attr("stroke-width", 2)
+                            .attr("stroke", colorLine)
+                            .attr("stroke-opacity", 0.8);
+                        svg.append("text")
+                            .attr("y", 0)
+                            .attr("x", d.row * w + labelWidth + TandGPadding + w / 2)
+                            .attr("class", "click")
+                            .attr("text-anchor", "middle")
+                            .attr("font-family", HMfont)
+                            .attr("font-size", "15px")
+                            .style("fill", colorLine)
+                            .text(d.date);
+                        /* var x2 = d.row * w + labelWidth + TandGPadding + w;
+                       var startRect = savedX1;
+                       var widthRect = Math.abs(x2-savedX1);
+                       	if (savedX1 > x2){
+							startRect = x2;
+                       	}
+                        svg.append("rect")
+                        	.attr("class","selection")
+                            .attr("x", startRect)
+                            .attr("y", startH)
+                            .attr("width", widthRect)
+                            .attr("height", totalH-startH)
+                            .style("fill",colorLine)
+                            .style("fill-opacity",0.1); 
+						*/
+                        clickHold = 1;
+                        updateInfoBoxHMap(d, 2);
+
+                    } else if (clickHold == 1) {
+                        svg.select("line.cl").remove();
+                        svg.select("line.cl").remove();
+                        svg.select("text.click").remove();
+                        svg.select("text.click").remove();
+                        //svg.select("rect.selection").remove();
+
+                        svg.append("line")
+                            .attr("class", "cl")
+                            .attr("x1", d.row * w + labelWidth + TandGPadding)
+                            .attr("y1", startH)
+                            .attr("x2", d.row * w + labelWidth + TandGPadding + w / 2)
+                            .attr("y2", totalH)
+                            .attr("stroke-width", 2)
+                            .attr("stroke", colorLine)
+                            .attr("stroke-opacity", 0.8);
+
+                        svg.append("text")
+                            .attr("y", 0)
+                            .attr("x", d.row * w + labelWidth + TandGPadding + w / 2)
+                            .attr("class", "click")
+                            .attr("text-anchor", "middle")
+                            .attr("font-family", HMfont)
+                            .attr("font-size", "15px")
+                            .style("fill", colorLine)
+                            .text(d.date);
+                        //savedX1 = d.row * w + labelWidth + TandGPadding;    
+                        clickHold = 0;
+                        updateInfoBoxHMap(d, 1);
+                    }
+                });
+        }
+
+        /*var imgs = svg.selectAll(".heatmap").data([0]);
+            imgs.enter()
+            .append("svg:image")
+            .attr("xlink:href", "Resource/ow_logo.png")
+            .attr("x", "10")
+            .attr("y", "0")
+            .attr("width", h)
+            .attr("height", h);
+
+        var imgs2 = svg.selectAll(".heatmap").data([0]);
+            imgs2.enter()
+            .append("svg:image")
+            .attr("xlink:href", "Resource/csgo_logo.jpg")
+            .attr("x", "0")
+            .attr("y", 2*h)
+            .attr("width", h+20)
+            .attr("height", h);
+
+        var imgs3 = svg.selectAll(".heatmap").data([0]);
+            imgs3.enter()
+            .append("svg:image")
+            .attr("xlink:href", "Resource/pubg_logo.png")
+            .attr("x", "0")
+            .attr("y", 4*h)
+            .attr("width", h+20)
+            .attr("height", h);
+
+        var imgs4 = svg.selectAll(".heatmap").data([0]);
+            imgs4.enter()
+            .append("svg:image")
+            .attr("xlink:href", "Resource/destiny_logo.jpg")
+            .attr("x", "0")
+            .attr("y", 6*h)
+            .attr("width", h)
+            .attr("height", h);*/
+
+        for (i = 0; i < 5; i++) {
+            var text = svg.selectAll(".heatmap").data([0]);
+            text.enter()
+                .append("svg:text")
+                .attr("text-anchor", "middle")
+                .attr("font-family", HMfont)
+                .attr("class", "gameLabel")
+                .attr("dy", ".30em")
+                .attr("x", (labelWidth + TandGPadding) / 2)
+                .attr("y", i * LH + h / 2 + startH)
+                .attr("fill-opacity", 0.95)
+                .text(games[i])
+                .style('fill', gameColors[i])
+                .style("font-size", "20px");
+        }
+    }
+
+    function plotStar(gameData, propertyAry) {}
+
+
+    prepareDataForStarPlot(gdata[0][1]);
+    plotStar();
+    var gD = prepareDataForHeatmap(gdata, "Peak Viewers")
+    plotHeatmap(gD);
+}
+
+// ============================================================
+// data processing
+queue()
+    .defer(d3.csv, "data/Overwatch.csv")
+    .defer(d3.csv, "data/CSGO.csv")
+    .defer(d3.csv, "data/PUBG.csv")
+    .defer(d3.csv, "data/Destiny.csv")
+    .defer(d3.csv, "data/Destiny2.csv")
+    .awaitAll(dataProcessing); //only function name is needed
